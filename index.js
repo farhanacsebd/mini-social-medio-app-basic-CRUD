711b2e4cf133eb7fb84b776838bb88efbd19e471
farhanacsebd <farhana.cse.bd@gmail.com>
2022-11-10 23:40:05 +0600

    handle error

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const form = document.getElementById('form');
+const input = document.getElementById('input')
 const msg = document.getElementById('msg');
 
 form.addEventListener('submit',function(e){
     e.preventDefault();
-    console.log('hello');
-})
+    formValidation()
+});
+
+
+function formValidation(){
+    if(input.value === ''){
+        msg.innerText = 'Post cannot be blank';
+    }
+    else{
+        console.log('success');
+    }
+}
